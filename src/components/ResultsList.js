99c5802e4da79bb19f3b78f4ee9f608426675e43
chaz.sherrill@codepoetllc.com <chaz.sherrill@codepoetllc.com>
2024-01-01 16:37:53 -0600

    finished up the app by hiding empty stuff

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native'
-import { withNavigation} from 'react-navigation'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+import { withNavigation } from 'react-navigation'
 import ResultsDetail from './ResultsDetail'
 
-const ResultsList  = ({title, results, navigation}) => {
+const ResultsList = ({ title, results, navigation }) => {
+
+	if (!results.length) {
+		return null
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>{title}</Text>
@@ -14,8 +19,8 @@ const ResultsList  = ({title, results, navigation}) => {
 				keyExtractor={(result) => result.id}
 				renderItem={({ item }) => {
 					return (
-						<TouchableOpacity onPress={() => navigation.navigate('ResultsShow', {id: item.id})}>
-							<ResultsDetail result={item}/>
+						<TouchableOpacity onPress={() => navigation.navigate('ResultsShow', { id: item.id })}>
+							<ResultsDetail result={item} />
 						</TouchableOpacity>
 					)
 				}}
@@ -25,8 +30,8 @@ const ResultsList  = ({title, results, navigation}) => {
 }
 
 const styles = StyleSheet.create({
-	title:{
-		fontSize: 18, 
+	title: {
+		fontSize: 18,
 		fontWeight: 'bold',
 		marginLeft: 15,
 		marginBottom: 5
